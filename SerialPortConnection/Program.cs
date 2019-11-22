12f15dbc3d9c987c2d1cd44d14d2203b6df4c63b
paulkli355 <paulina.klimanek@gmail.com>
2019-11-22 21:39:21 +0100

    odebranie informacji w VS z RS

---
+++ SerialPortConnection/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialPortConnection
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        static void Main(string[] args)
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM7";
+            _serialPort.BaudRate = 9600;
+
+            try
+            {
+                _serialPort.Open();
+                while (true)
+                {
+                    string answer = _serialPort.ReadExisting();
+                    Console.WriteLine(answer);
+                    //string podzial = answer.Split(";");
+                    Thread.Sleep(1000);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.GetType().Name + ": Port nie gotowy!");
+            }
+            finally
+            {
+                Console.Read();
+            }
+
+        }
+    }
+
+}
