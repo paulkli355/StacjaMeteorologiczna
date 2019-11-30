34a4264c863b9e5cf7249e0895d4bb96c50b1aaa
paulkli355 <paulina.klimanek@gmail.com>
2019-11-30 21:54:06 +0100

    update EF Database

--- SerialPortConnection/Program.cs
+++ SerialPortConnection/Program.cs
@@ -12,12 +12,13 @@ namespace SerialPortConnection
 {
     class Program
     {
-     
         // kolejnosc w lancuchu 
         //PM1 ; PM25 ; PM10 ; temperatura ; wilgotnosc
         static SerialPort _serialPort;
         static void Main(string[] args)
         {
+            
+
             _serialPort = new SerialPort();
             _serialPort.PortName = "COM6";
             _serialPort.BaudRate = 9600;
@@ -26,99 +27,79 @@ namespace SerialPortConnection
             
             string answer;
             string[] wartosci;
-
-            for (; ; )
-            {
-
+            //using (PomiaryContext context = new PomiaryContext())
+            //{
                 for (; ; )
                 {
-                    
-                    Thread.Sleep(600);
-                    answer = _serialPort.ReadExisting();
-                    wartosci = answer.Split(';');
-                    Console.WriteLine("");
-                    if (wartosci.Length > 5)
-                        break;
+
+                    for (; ; )
+                    {
+
+                        Thread.Sleep(600);
+                        answer = _serialPort.ReadExisting();
+                        wartosci = answer.Split(';');
+                        Console.WriteLine("");
+                        if (wartosci.Length > 5)
+                            break;
+                    }
+
+                    double pm1;
+                    double pm25;
+                    double pm10;
+                    double temp;
+                    double wilg;
+
+                    Console.WriteLine("pm1: {0}", wartosci[0]);
+                    Console.WriteLine("pm25: {0}", wartosci[1]);
+                    Console.WriteLine("pm10: {0}", wartosci[2]);
+                    Console.WriteLine("temp: {0}", wartosci[3]);
+                    Console.WriteLine("wilg: {0}", wartosci[4]);
+
+                    pm1 = double.Parse(wartosci[0]);
+                    pm25 = double.Parse(wartosci[1]);
+                    pm10 = double.Parse(wartosci[2]);
+                    temp = double.Parse(wartosci[3]);
+                    wilg = double.Parse(wartosci[4]);
+                    //Thread.Sleep(1000);
+
+                    Console.WriteLine("pm1: {0}", pm1);
+                    Console.WriteLine("pm25: {0}", pm25);
+                    Console.WriteLine("pm10: {0}", pm10);
+                    Console.WriteLine("temp: {0}", temp);
+                    Console.WriteLine("wilg: {0}", wilg);
+
+
+                    //INSERT to MeteoDB database
+                    //Pomiary pomiar = new Pomiary
+                    //{
+                    //    DataCzas = DateTime.Now,
+                    //    PM1 = pm1,
+                    //    PM25 = pm25,
+                    //    PM10 = pm10,
+                    //    Temperatura = temp,
+                    //    Wilgotnosc = wilg
+                    //};
+                    //context.Pomiaries.Add(pomiar);
+                    //context.SaveChanges();
+
+                    //SELECT from MeteoDB database
+                    //Pomiary pomiar2 = context.Pomiaries.FirstOrDefault(x => x.Temperatura >= 0);
+                    //Console.WriteLine(pomiar2.PomiarID);
+                    //Console.Read();
+
+                    //ALTERING data
+                    //pomiar2.Wilgotnosc = 72;
+                    //context.SaveChanges();
+                    //Console.WriteLine(pomiar2);
+
+                    //DELETE from MeteoDB database
+                    //Pomiary pomiar3 = context.Pomiaries.FirstOrDefault(x => x.Temperatura == 32);
+                    //context.Pomiaries.Remove(pomiar3);
+                    //context.SaveChanges();
+
+
                 }
-
-                int pm1;
-                int pm25;
-                int pm10;
-                int temp;
-                int wilg;
-
-                pm1 = int.Parse(wartosci[0]);
-                pm25 = int.Parse(wartosci[1]);
-                pm10 = int.Parse(wartosci[2]);
-                temp = int.Parse(wartosci[3]);
-                wilg = int.Parse(wartosci[4]);
-                //Thread.Sleep(1000);
-
-                Console.WriteLine("pm1: {0}",pm1);
-                Console.WriteLine("pm25: {0}", pm25);
-                Console.WriteLine("pm10: {0}", pm10);
-                Console.WriteLine("temp: {0}", temp);
-                Console.WriteLine("wilg: {0}", wilg);
-
-            }
-
-            ////SQL Connection 
-            //try
-            //{
-            //    string connecting = "datasource=DELL/PAULINA;port=3306;username=sa;password=paulina1";
-            //    string Query = "insert into database.MeteoDB(PomiarID, Kategoria,DataCzas,Wartosc) values()";
-
-            //    SqlConnection polaczenie = new SqlConnection(@"Data source=DELL\PAULINA;
-            //                                                 database=MeteoDB;
-            //                                                 User id=sa;
-            //                                                 Password=paulina1;");
-
-            //    polaczenie.Open();
-            //    SqlCommand komendaSQL = polaczenie.CreateCommand();
-            //    komendaSQL.CommandText = "SELECT * FROM adres";
-            //    SqlDataReader czytnik = komendaSQL.ExecuteReader();
-
-            //    Console.WriteLine("Wiersze tabeli:");
-            //    while (czytnik.Read())
-            //    {
-            //        Console.WriteLine(czytnik["nazwisko"].ToString() + "   " + czytnik["imie"].ToString() + "   " + czytnik["adres"].ToString() + "   " + czytnik["kodp"].ToString() + "   " + czytnik["poczta"].ToString());
-            //    }
-            //    czytnik.Close();
-            //    polaczenie.Close();
-
-            //    Console.ReadKey();
             //}
-
-            //catch (SqlException e)
-            //{
-            //    Console.WriteLine("Wystąpił nieoczekiwany błąd!");
-            //    Console.WriteLine(e.Message);
-            //    Console.ReadKey();
-            //}
-        }
-
-        void SaveToSQL()
-        {
-            //DateTime czas = DateTime.Now;
-            //string constring = "Data Source=DELL/PAULINA;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-            //SqlConnection conection = new SqlConnection(constring);
-            //conection.Open();
-            //string Query = "insert into Użytkownik.dbo.Rejestracja(UserName,Password,FirstName,LastName,Weight,Height,Sex) values ('" + pm1 + "','" + pm25 + "','" + pm10 + "','" + temp + "', '" + wilg+ "', '"')";
-
-            //SqlCommand cmd = new SqlCommand(Query, conection);
-            //int i = cmd.ExecuteNonQuery();
-
-            //if (i != 0)
-            //{
-            //    Console.WriteLine("Saved");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Error has occured!");
-            //}
-
-            
-            
         }
 
     }
